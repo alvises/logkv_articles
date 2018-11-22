da1777d86e05caaa4fdfff9402bc5baecf12eac5
Alvise Susmel <alvise@poeticoding.com>
2018-11-22 16:08:37 +0000

    index update/3 doc about synchronicity

--- lib/logkv/index.ex
+++ lib/logkv/index.ex
@@ -1,31 +1,33 @@
 defmodule LogKV.Index do
-	use GenServer
+  use GenServer
 
-	def start_link(:empty) do
-		GenServer.start_link(__MODULE__, :empty, name: __MODULE__)
-	end
+  def start_link(:empty) do
+    GenServer.start_link(__MODULE__, :empty, name: __MODULE__)
+  end
 
-	# empty index
-	def init(:empty), do: {:ok, %{}}
-	
+  # empty index
+  def init(:empty), do: {:ok, %{}}
 
-	@doc ~S"""
-	Updates the index for a specific `key`. It saves the `offset` and `size` 
-	of the value in the log file.
-	
-	## Examples
+  @doc ~S"""
+  Updates the index for a specific `key`. It saves the `offset` and `size` 
+  of the value in the log file.
 
-		iex> {:ok, _index} = LogKV.Index.start_link(:empty)
-		iex> LogKV.Index.update("my_key", 0, 10)
-		:ok
-	
-	"""
-	def update(key, offset, size) do
-		GenServer.call(__MODULE__, {:set, key, offset, size})
-	end
+  The update is made doing a `GenServer.call`, so it's synchronous and 
+  you have then the guarantee that the next `lookup` message will be able to 
+  get the updated offset/size for that key.
 
-	def handle_call({:set, key, offset, size}, _from, index_map) do
-		{:reply, :ok, Map.put(index_map, key, {offset, size})}
-	end
+  ## Examples
 
+  	iex> {:ok, _index} = LogKV.Index.start_link(:empty)
+  	iex> LogKV.Index.update("my_key", 0, 10)
+  	:ok
+
+  """
+  def update(key, offset, size) do
+    GenServer.call(__MODULE__, {:set, key, offset, size})
+  end
+
+  def handle_call({:set, key, offset, size}, _from, index_map) do
+    {:reply, :ok, Map.put(index_map, key, {offset, size})}
+  end
 end
