11af7c8d116a13a30ecdce7135950fecac8b1300
Alvise Susmel <alvise@poeticoding.com>
2018-11-22 18:35:17 +0000

    index lookup function

--- lib/logkv/index.ex
+++ lib/logkv/index.ex
@@ -1,6 +1,11 @@
 defmodule LogKV.Index do
   use GenServer
 
+  @doc ~S"""
+  At the beginning the index will be empty. The first super-simple implementation 
+  doesn't recover the index. For this version we just need one `Writer` and one `Index` and then they
+  both have a name
+  """
   def start_link(:empty) do
     GenServer.start_link(__MODULE__, :empty, name: __MODULE__)
   end
@@ -27,7 +32,38 @@ defmodule LogKV.Index do
     GenServer.call(__MODULE__, {:update, key, offset, size})
   end
 
+  @doc ~S"""
+  Gets a `tuple` with `offset` and `size`  for a specific `key`.
+
+  ### Examples
+
+    iex> {:ok, _index} = LogKV.Index.start_link(:empty)
+    iex> {:error, :not_found} = LogKV.Index.lookup("not_existing_key")
+    iex> LogKV.Index.update("btc",10,6)
+    :ok
+    iex> LogKV.Index.lookup("btc")
+    {:ok, {10, 6}}
+
+
+  """
+  def lookup(key) do
+    GenServer.call(__MODULE__, {:lookup, key})
+  end
+
   def handle_call({:update, key, offset, size}, _from, index_map) do
     {:reply, :ok, Map.put(index_map, key, {offset, size})}
+  end
+
+  def handle_call({:lookup, key}, _from, index_map) do
+    {:reply, get_key_offset_size(key, index_map), index_map}
+  end
+
+  # if the key exists returns {:ok, {offset, size}} if the keys is found.
+  # if key is not found returns {:error, :not_found}
+  defp get_key_offset_size(key, index_map) do
+    case Map.get(index_map, key) do
+      {_offset, _size} = offset_size -> {:ok, offset_size}
+      nil -> {:error, :not_found}
+    end
   end
 end
