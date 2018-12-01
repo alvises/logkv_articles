fe4a8d193ac9a47f3605e73292ac3a2781662ce7
Alvise Susmel <alvise@poeticoding.com>
2018-12-01 17:08:14 +0000

    writer function to create a binary entry for timestamp, key, value with sizes

--- lib/logkv/writer.ex
+++ lib/logkv/writer.ex
@@ -2,9 +2,14 @@ defmodule LogKV.Writer do
   use GenServer
 
   @moduledoc ~S"""
-  In this first version, we just want to see how the mapping between the `Writer` and the `Index` works.
+  Part-1: In this first version, we just want to see how the mapping between the `Writer` and the `Index` works.
   We just open a file, appending on it using just one writer with one file, without using segments and 
   compaction (which we'll see in future implementations).
+
+  Part-2: We write the key and value together, with their sizes, so we can recover the index from the log file.
+  We use the same entry schema of the bitcask design you can find here: http://basho.com/wp-content/uploads/2015/05/bitcask-intro.pdf, 
+  we only skeep CRC error check, maybe we do it in an appendix of this article.
+
   """
 
   @doc ~S"""
@@ -52,4 +57,48 @@ defmodule LogKV.Writer do
     new_state = %{state | current_offset: current_offset + size}
     {:reply, {:ok, {current_offset, size}}, new_state}
   end
+
+  
+  @doc ~S"""
+  Part-2: Tansforms the integer representing the size of the key and the value in bytes.
+  The size of the key is represented by a 16bit unsigned integer,  this means can be maximum around 65kb, which for this example is more then enough.
+  We use a 32bit unsigned int to represent the value size, which means the maximum size of the value is 4.29GB. If someone needs to use more 4GB maybe it's
+  better to use the filesystem directly as a kv store.
+  *big-unsigned-integer-size(16)* means the integer is converted to a binary of 2bytes big endian order. 
+  If you want to know what endianess is, please look here: https://en.wikipedia.org/wiki/Endianness
+
+    ## Examples
+
+      iex> {_data, key_size, value_size} = LogKV.Writer.kv_to_binary(:os.system_time(:millisecond), "key", "value")
+      iex> key_size
+      <<0, 3>>
+      iex> value_size
+      <<0, 0, 0, 5>>
+
+  """
+  def kv_to_binary(timestamp,key, value) do
+
+    #conversion of an integer to big endian 16bit unisigned integer. 
+    # 
+    # << our_int :: conversion from integer to binary >>
+
+    # timestamp = :os.system_time(:millisecond)
+    timestamp_data = <<timestamp::big-unsigned-integer-size(64)>>
+
+    key_size = <<byte_size(key)::big-unsigned-integer-size(16)>>
+    value_size = <<byte_size(value)::big-unsigned-integer-size(32)>>
+
+    # sizes_data is a 6bytes binary with first 2 bytes the key size and then 4 bytes for the value size.
+    sizes_data = <<key_size::binary, value_size::binary>>
+
+    #kv_data is just the concatenation of the key and value
+    kv_data = <<key::binary, value::binary>>
+
+    #we then create a single entry which comprehends timestamp, key and values sizes, key and the value.
+    data = <<timestamp_data::binary, sizes_data::binary, kv_data::binary>>
+    
+    #we then return a tuple with the entry data, key size and value size
+    {data, key_size, value_size}
+  end
+
 end
